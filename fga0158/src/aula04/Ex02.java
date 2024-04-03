a9e470273b5d09ac8884eb66bb09f48dbb6539ee
Joselito Prado <joselito.prado.marques@gmail.com>
2024-04-03 19:32:36 -0300

    Ex02 otimizado com funções

--- fga0158/src/aula04/Ex02.java
+++ fga0158/src/aula04/Ex02.java
@@ -23,32 +23,34 @@ public class Ex02 {
 		int[] vetor = new int[x];
 		
 		for(int i = 0; i < x; i++) {
-			int index = i + 1;
-			System.out.print("Digite um valor para o elemento " + index + ": ");
-			int value = entrada.nextInt();
-			vetor[i] = value;
+			escreverValor(entrada, i, vetor);
 		}
+		entrada.close();
 		int[] sorted_vetor = vetor.clone();
 		Arrays.sort(sorted_vetor);
 		
-		System.out.print("Vetor original: [ ");
-		for(int i = 0; i < vetor.length; i++) {
-			System.out.print(vetor[i]);
-			if (i < vetor.length -1) 
-				System.out.print(", ");;
-		}
-		System.out.println(" ]");
-		
-		System.out.print("Vetor ordenado: [ ");
-		for(int i = 0; i < vetor.length; i++) {
-			System.out.print(sorted_vetor[i]);
-			if (i < vetor.length -1) 
-				System.out.print(", ");;
-		}
-		System.out.print(" ]");
-		
-		entrada.close();
+		System.out.print("Vetor original: ");
+		imprimirVetor(vetor);
+		System.out.print("Vetor ordenado: ");
+		imprimirVetor(sorted_vetor);
 	}
+
+    public static void imprimirVetor(int[] vetor) {
+        System.out.print("[ ");
+        for(int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i]);
+            if (i < vetor.length - 1) 
+                System.out.print(", ");
+        }
+        System.out.println(" ]");
+    }
+    
+    public static void escreverValor(Scanner entrada, int i, int[] vetor) {
+		int index = i + 1;
+		System.out.print("Digite um valor para o elemento " + index + ": ");
+		int value = entrada.nextInt();
+		vetor[i] = value;
+    }
 	
 
 }
